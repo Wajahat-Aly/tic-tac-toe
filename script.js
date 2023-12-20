57f9e2d0be1d661ee771b2bb956f4db35b05d87e
Wajahat-ALI <aliwajahat812@gmail.com>
2023-12-20 22:16:29 +0500

    first commivt

--- script.js
+++ script.js
@@ -16,4 +16,43 @@ function initializeTicTacToe() {
     // Your game logic implementation here
   }
 
+  let boxes=document.querySelectorAll("#boxes");
+  let resetbtn=document.querySelector(".button-rest");
+
+   let turn0=true;//player O
+
+  let winingsptrn=[[0, 1, 2], // Top row
+  [3, 4, 5], // Middle row
+  [6, 7, 8], // Bottom row
+
+  // Vertical lines
+  [0, 3, 6], // Left column
+  [1, 4, 7], // Middle column
+  [2, 5, 8], // Right column
+
+  // Diagonal lines
+  [0, 4, 8], // Top-left to bottom-right
+  [2, 4, 6], // Top-right to bottom-left
+];
+
+
+boxes.forEach(boxes=>{
+  boxes.addEventListener("click",()=>{ console.log("box Was clicked");
+  if(turn0){
+    boxes.innerText="Great";
+    turn0=false;
+  }
+  else{
+    boxes.innerText="Gretat";
+    turn0=true;
+  }
+  
+}
+ 
+   
+  )
+})
+
+
+
 });
